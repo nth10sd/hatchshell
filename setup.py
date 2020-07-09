28db02e3b9d055bb64b50dbcc172f3bfe430b75d
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-07-08 21:55:26 -0700

    Remove Python 3.7 classifier since it is not supported officially for now.

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Testing",
