79c512a209cfa1a76e9585c67a29dbf766f31813
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-12-06 03:40:11 -0800

    Fix linter error with docs config file

--- docs/source/conf_correct.py
+++ docs/source/conf_correct.py
@@ -15,10 +15,9 @@
 import os
 import sys
 
+import hatchshell  # noqa: E402,I001
 
 sys.path.insert(0, os.path.abspath("../.."))
-
-import hatchshell  # noqa: E402,I001
 
 # -- Project information -----------------------------------------------------
 
