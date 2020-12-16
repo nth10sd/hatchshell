2a55a9cbf08c0cee9c59a9edfe8103b8daf651fc
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-12-16 03:11:36 -0800

    Fix more linter errors

--- docs/source/conf_correct.py
+++ docs/source/conf_correct.py
@@ -14,8 +14,9 @@
 #
 import os
 import sys
+from typing import List
 
-import hatchshell  # noqa: E402,I001
+import hatchshell
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -44,7 +45,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []  # type: ignore
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
